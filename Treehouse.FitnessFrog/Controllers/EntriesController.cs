54520a5762a147e9b8e528734b21ad9215d20680
Thomas Fletcher Windows Machine <tom_fletcher@hotmail.co.uk>
2019-06-30 17:08:32 +0100

    test

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -41,7 +41,25 @@ namespace Treehouse.FitnessFrog.Controllers
 
         public ActionResult Add()
         {
-            return View();
+            var entry = new Entry()
+            {
+                Date = DateTime.Today
+            };
+
+            return View(entry);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Entry entry)
+        {
+            if(ModelState.IsValid)
+            {
+                _entriesRepository.AddEntry(entry);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(entry);
         }
 
         public ActionResult Edit(int? id)
